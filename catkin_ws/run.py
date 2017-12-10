7087a9db121beef73d154e81ef87aa2e7467198b
julinas <julinas@traclabs.com>
2017-12-10 11:47:58 -0800

    set up AC; training to stand

--- catkin_ws/run.py
+++ catkin_ws/run.py
@@ -1,262 +1,254 @@
 import subprocess
-import numpy as np
+import numpy as np 
 import time
 import re
 import random
-from collections import deque
+import sys
+import os
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input
-from keras.layers.merge import Add, Multiply
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.optimizers import Adam
-import keras.backend as K
-
-import tensorflow as tf
+from keras.models import load_model
 
 class ActorCritic:
-	def __init__(self, env, sess):
-		self.env = env
-		self.sess = sess
+    def __init__(self, env):
+        self.env = env
+        self.learning_rate = 0.001
+        self.epsilon = 1.0
+        self.epsilon_decay = .995
+        self.gamma = .95
+        self.memory = None
 
-		self.learning_rate = 0.001
-		self.epsilon = 1.0
-		self.epsilon_decay = .995
-		self.gamma = .95
-		self.tau = .125
-		self.memory = deque(maxlen=2000)
+        self.actor_model = self.create_actor_model()
+        self.critic_model = self.create_critic_model()
 
-		self.actor_state_input, self.actor_model = self.create_actor_model()
-		_, self.target_actor_model = self.create_actor_model()
+    def create_actor_model(self):
+        model = Sequential()
+        model.add(Dense(13, activation='relu', input_shape=(13, )))
+        model.add(Dense(26, activation='relu'))
+        model.add(Dense(13, activation='relu'))
+        model.add(Dense(10, activation='relu'))
 
-		self.actor_critic_grad = tf.placeholder(tf.float32, 
-			[None, self.env.action_space_shape[0]])
+        adam = Adam(lr=self.learning_rate)
+        model.compile(loss='mse', optimizer=adam)
+        return model
 
-		actor_model_weights = self.actor_model.trainable_weights
-		self.actor_grads = tf.gradients(self.actor_model.output,
-			actor_model_weights, -self.actor_critic_grad)
+    def create_critic_model(self):
+        model = Sequential()
+        model.add(Dense(23, activation='relu', input_shape=(23, )))
+        model.add(Dense(46, activation='relu'))
+        model.add(Dense(23, activation='relu'))
+        model.add(Dense(1, activation='relu'))
 
-		grads = zip(self.actor_grads, actor_model_weights)
-		self.optimize = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(grads)
+        adam = Adam(lr=self.learning_rate)
+        model.compile(loss='mse', optimizer=adam)
+        return model
 
-	def create_actor_model(self):
-		state_input = Input(shape=self.env.observation_space.shape)
-		h1 = Dense(24, activation='relu')(state_input)
-		h2 = Dense(48, activation='relu')(h1)
-		h3 = Dense(24, activation='relu')(h2)
-		output = Dense(self.env.action_space_shape[0], activation='relu')(h3)
+    def train(self, batch_size):
+        # samples = None
+        # # if len(self.memory) < batch_size:
+        # #     samples = self.memory
+        # # else:
+        # indices = np.random.randint(0, len(self.memory))
+        # samples = self.memory[indices]
+        samples = self.memory
+        self._train_critic(samples)
+        self._train_actor(samples)
 
-		model = Model(input=state_input, output=output)
-		adam = Adam(lr=0.001)
-		model.compile(loss='mse', optimizer=adam)
-		return state_input, model
+    def _train_critic(self, samples):
+        x = samples[:, 0:23] # cur_state, action
+        y = samples[:, 24] # new_reward
+        self.critic_model.train_on_batch(x, y)
 
-	def create_critic_model(self):
-		state_input = Input(shape=self.env.observation_space.shape)
-		state_h1 = Dense(24, activation='relu')(state_input)
-		state_h2 = Dense(48)(state_h1)
+    def _train_actor(self, samples):
+        x = samples[:, 0:13] # cur_state
+        y = samples[:, 13:23] # action
+        self.actor_model.train_on_batch(x, y)
 
-		action_input = Input(shape=self.env.action_space_shape)
-		action_h1 = Dense(48)(action_input)
+    def clear_memory(self):
+        self.memory = None
 
-		merged = Add()([state_h2, action_h1])
-		merged_h1 = Dense(24, activation='relu')(merged)
-		output = Dense(1, activation='relu')(merged_h1)
-		model = Model(input=[state_input, action_input], output=output)
-		adam = Adam(lr=0.001)
-		model.compile(loss='mse', optimizer=adam)
-		return state_input, action_input, model
+    def remember(self, cur_state, action, reward, new_reward):
+        memory = np.concatenate((cur_state, action), axis=1)
+        memory = np.concatenate((memory, np.asarray([reward, new_reward]).reshape((1, 2))), axis=1)
+        # memory = memory.reshape((1, 25))
+        # memory = memory.reshape((1, memory.shape[0]))
+        if self.memory is None:
+            self.memory = memory
+        else:
+            self.memory = np.concatenate((self.memory, memory))
 
-	def train(self):
-		batch_size = 1
-		if len(self.memory) < batch_size:
-			return
-
-		rewards = []
-		samples = random.sample(self.memory, batch_size)
-		self._train_critic(samples)
-		self._train_actor(samples)
-
-	def _train_critic(self, samples):
-		for sample in samples:
-			cur_state, action, reward, new_state, done = sample
-			if not done:
-				target_action = self.target_actor_model.predict(new_state)
-				future_rewards = self.target_critic_model.predict(
-					[new_state, target_action])[0][0]
-				rewards += self.gamma * future_reward
-			self.critic_model.fit([cur_state, action], reward, verbose=0)
-
-	def remember(self, cur_state, action, reward, new_state, done):
-		self.memory.append([cur_state, action, reward, new_state, done])
-
-	def _train_actor(self, samples):
-		for sample in samples:
-			cur_state, action, reward, new_state, _ = sample
-			predicted_action = self.actor_model.predict(cur_state)
-			grads = self.sess.run(self.critic_grads, feed_dict={
-				self.critic_state_input: cur_state,
-				self.critic_action_input: predicted_action
-			})[0]
-
-			self.sess.run(self.optimize, feed_dict={
-				self.actor_state_input: cur_state,
-				self.actor_critic_grad: grads
-			})
-
-	def _update_actor_target(self):
-		actor_model_weights = self.actor_model.get_weights()
-		actor_target_weights = self.target_critic_model.get_weights()
-
-		for i in range(len(actor_target_weights)):
-			actor_target_weights[i] = actor_model_weights[i]
-		self.target_critic_model.set_weights(actor_target_weights)
-
-	def act(self, cur_state):
-		self.epsilon *= self.epsilon_decay
-		if np.random.random() < self.epsilon:
-			return self.env.sample_action()
-		return self.actor_model.predict(cur_state)
+    def act(self, cur_state):
+        self.epsilon *= self.epsilon_decay
+        if np.random.random() < self.epsilon:
+            return self.env.sample_action().reshape((1, 10))
+        return self.actor_model.predict(cur_state).reshape((1, 10))
 
 class Env:
-	def __init__(self):
-		self.observation_space = np.zeros() 
-		self.observation_space_shape = self.observation_space.shape
-		self.joint_names = ['joint_5l6l', 'joint_5r6r', 'joint_45l', 'joint_45r', 'joint_01l', 'joint_01r', 'joint_1l2l', 'joint_1r2r', 'joint_2l9l', 'joint_2r9r']
-		self.action_space_shape = [len(self.joint_names)]
+    def __init__(self):
+        self.joint_names = ['joint_5l6l', 'joint_5r6r', 'joint_45l', 'joint_45r', 'joint_01l', 'joint_01r', 'joint_1l2l', 'joint_1r2r', 'joint_2l9l', 'joint_2r9r']
+        self.action_space_shape = [len(self.joint_names)]
+        self.state_shape = (13, )
+        self.action_shape = (10, )
 
-	def spawn_figure(self):
-		subprocess.check_call(["rosrun", "gazebo_ros", "spawn_model", "-database", "my_robot", "-gazebo",
-			"-model", "figure", "-y", "0", "-x", "0"])
+    def spawn_figure(self):
+        subprocess.check_call(["rosrun", "gazebo_ros", "spawn_model", "-database", "my_robot", "-gazebo",
+            "-model", "figure", "-y", "0", "-x", "0"])
 
-	def pause(self):
-		subprocess.call(["rosservice", "call", "gazebo/pause_physics"])
+    def pause(self):
+        subprocess.call(["rosservice", "call", "gazebo/pause_physics"])
 
-	def unpause(self):
-		subprocess.call(["rosservice", "call", "gazebo/unpause_physics"])
+    def unpause(self):
+        subprocess.call(["rosservice", "call", "gazebo/unpause_physics"])
 
-	def reset(self):
-		subprocess.check_call(["rosservice", "call", "gazebo/reset_simulation"])
-		return get_model_state()
+    def reset(self):
+        subprocess.check_call(["rosservice", "call", "gazebo/reset_simulation"])
+        return self.get_model_state()
 
-	def get_model_state(self):
-		output = subprocess.check_output(["rosservice", "call", "gazebo/get_model_state", "figure", "world"])
-		return parse_state_str(output)
+    def get_model_state(self):
+        output = subprocess.check_output(["rosservice", "call", "gazebo/get_model_state", "figure", "world"])
+        return parse_state_str(output, True)
 
-	def get_link_state(self, link_name):
-		output = subprocess.check_output(["rosservice", "call", "gazebo/get_link_state", link_name, "world"])
-		return parse_state_str(output)
+    def get_link_state(self, link_name):
+        output = subprocess.check_output(["rosservice", "call", "gazebo/get_link_state", link_name, "world"])
+        return parse_state_str(output, False)
 
-	def sample_action(self):
-		return np.random.standard_normal(size=(self.action_space_shape[0], )) * 10
+    def sample_action(self):
+        return (np.random.random([self.action_space_shape[0]]) - 0.5) * 20
 
-	def clear_joint_forces(self):
-		subprocess.checkout_output(["rosservice", "call", "gazebo/clear_joint_forces", 
-			"{joint_name: %(joint_list)s}" % {'joint_list': joint_names}])
+    def clear_joint_forces(self):
+        subprocess.checkout_output(["rosservice", "call", "gazebo/clear_joint_forces", 
+            "{joint_name: %(joint_list)s}" % {'joint_list': joint_names}])
 
-	def step(self, action):
-		self.unpause()
-		for i in range(len(action)):
-			subprocess.call(["rosservice", "call", "gazebo/apply_joint_effort", """joint_name: '%(joint_name)s'
-				effort: %(effort)d
-				start_time:
-					secs: 0
-					nsecs: 0
-				duration:
-					secs: 0
-					nsecs: 1000000""" % {'joint_name': self.joint_names[i], 'effort': action[i]}])
-		self.pause()
+    def step(self, action):
+        with open(os.devnull, 'w') as f:
+            for i in range(len(action[0])):
+            # print "trying", i
+                subprocess.call(["rosservice", "call", "gazebo/apply_joint_effort", """joint_name: '%(joint_name)s'
+effort: %(effort)d
+start_time:
+    secs: 0
+    nsecs: 0
+duration:
+    secs: 0
+    nsecs: 100000000""" % {'joint_name': self.joint_names[i], 'effort': action[0][i]}], stdout=f)
+        self.unpause()
+        time.sleep(0.1)
+        self.pause()
+        # print "paused again"
+        model_state = self.get_model_state()
+        # print "got model state"
+        # standing
+        reward, done = standing_objective(self)
+        # print "calculated objective"
+        return model_state, reward, done
 
-		return get_model_state()
+def parse_state_str(str, isModel):
+    l = str.split()
+    match = np.asarray(l)
+    if isModel:
+        match = match[[13, 15, 17, 20, 22, 24, 26, 30, 32, 34, 37, 39, 41]]
+    else:
+        match = match[[6, 8, 10, 13, 15, 17, 19, 23, 25, 27, 30, 32, 34]]
+    # print match
+    # print match.shape
+    return match.astype(np.float).flatten().reshape((1, 13))
+        # match = m.groups(1)
+        # print match
+        # sys.stdout.flush()
+        # return np.asarray(match).astype(np.float)
+        # return {
+        #   'pose': {
+        #       'position': {
+        #           'x': float(match[0]),
+        #           'y': float(match[1]),
+        #           'z': float(match[2])
+        #       },
+        #       'orientation': {
+        #           'x': float(match[3]),
+        #           'y': float(match[4]),
+        #           'z': float(match[5]),
+        #           'w': float(match[6])
+        #       }
+        #   },
+        #   'twist': {
+        #       'linear': {
+        #           'x': float(match[7]),
+        #           'y': float(match[8]),
+        #           'z': float(match[9])
+        #       },
+        #       'angular': {
+        #           'x': float(match[10]),
+        #           'y': float(match[11]),
+        #           'z': float(match[12])
+        #       }
+        #   }
+        # }
 
+def standing_objective(env):
+    head_state = env.get_link_state("link8")
+    # print head_state
+    loss = 3.75 - head_state[0, 2]
+    # loss = np.sum(np.square(model_state))
+    done = True if loss > 1 else False
+    return -loss, done
 
-
-def parse_state_str(str):
-	m = re.match("[\s\S]*x:\ ([\S]*)[\s\S]*y:\ ([\S]*)[\s\S]*z:\ ([\S]*)[\s\S]*x:\ ([\S]*)[\s\S]*y:\ ([\S]*)[\s\S]*z:\ ([\S]*)[\s\S]*w:\ ([\S]*)[\s\S]*x:\ ([\S]*)[\s\S]*y:\ ([\S]*)[\s\S]*z:\ ([\S]*)[\s\S]*x:\ ([\S]*)[\s\S]*y:\ ([\S]*)[\s\S]*z:\ ([\S]*)", str)
-	if m:
-		match = m.groups(1)
-		return np.asarray(match)
-		# return {
-		# 	'pose': {
-		# 		'position': {
-		# 			'x': float(match[0]),
-		# 			'y': float(match[1]),
-		# 			'z': float(match[2])
-		# 		},
-		# 		'orientation': {
-		# 			'x': float(match[3]),
-		# 			'y': float(match[4]),
-		# 			'z': float(match[5]),
-		# 			'w': float(match[6])
-		# 		}
-		# 	},
-		# 	'twist': {
-		# 		'linear': {
-		# 			'x': float(match[7]),
-		# 			'y': float(match[8]),
-		# 			'z': float(match[9])
-		# 		},
-		# 		'angular': {
-		# 			'x': float(match[10]),
-		# 			'y': float(match[11]),
-		# 			'z': float(match[12])
-		# 		}
-		# 	}
-		# }
-
-def standing_loss(model_state):
-	return np.sum(model_state)
+def sitting_objective(model_state):
+    pass
 
 def main():
-	env = Env()
-	env.pause()
-	env.spawn_figure()
-	print env.get_model_state()
+    env = Env()
+    env.pause()
+    env.spawn_figure()
 
-	sess = tf.Session()
-	K.set_session(sess)
-	actor_critic = ActorCritic(env, sess)
+    AC = ActorCritic(env)
 
-	num_trials = 10000
-	trial_len = 500
+    num_trials = 20 #1000
+    trial_len = 200
+    for j in range(num_trials):
+        cur_state = env.reset()
+        AC.clear_memory()
+        for i in range(trial_len):
+            action = AC.act(cur_state)
+            print i
+            new_state, reward, done = env.step(action)
+            if done:
+                print "done"
+                print env.get_model_state()
+                break
 
-	cur_state = env.reset()
-	action = env.sample_action()
-	while True:
-		# env.render() # update environ variables?
-		# cur_state = cur_state.reshape((1, env.observation_space.shape[0]))
-		action = actor_critic.act(cur_state)
-		action = action.reshape((1, env.action_space_shape[0]))
+            target_action = AC.actor_model.predict(new_state).reshape((1, 10))
+            new_reward = AC.critic_model.predict(
+                np.concatenate((new_state, target_action), axis=1))
+            new_reward = reward + AC.gamma * new_reward
 
-		new_state, reward, done = env.step(action)
-		new_state = new_state.reshape((1, env.observation_space.shape[0]))
+            AC.remember(cur_state, action, reward, new_reward)
+            AC.train(i)
+            cur_state = new_state
+    AC.actor_model.save_weights("actor_model.h5")
+    AC.critic_model.save_weights("critic_model.h5")
 
-		actor_critic.remember(cur_state, action, reward, new_state, done)
-		actor_critic.train()
+def validate():
+    actor_model = load_model("actor_model.h5")
+    env = Env()
+    env.pause()
+    env.spawn_figure()
 
-		cur_state = new_state
+    num_trials = 10
+    trial_len = 200
+    for j in range(num_trials):
+        cur_state = env.reset()
+        rewards = []
+        for i in range(trial_len):
+            action = actor_model.predict(cur_state).reshape((1, 10))
+            new_state, reward, done = env.step(action)
+            if done:
+                print "done"
+                break
 
+            rewards.append(reward)
+            cur_state = new_state
+        print "rewards for trial", j, rewards
 main()
-
-
-# child = subprocess.call(["nohup", "roslaunch", "my_robot_control", "my_robot.launch", "&"])
-# child.send_signal(signal_SIGINT)
-
-# import rospy
-# import roslaunch
-
-
-# print rospy.get_ros_root()
-# rospy.init_node('en_Mapping', anonymous=True)
-# # rospy.on_shutdown(self.shutdown)
-
-# print rospy.get_rostime()
-
-# uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-# roslaunch.configure_logging(uuid)
-# launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/julinas/catkin_ws/src/my_robot_control/launch/my_robot.launch"])
-# launch.start()
-
-# launch.shutdown()
-
-# rospy.init_node('my_node')
-# print rospy.get_rostime()
+# validate()
